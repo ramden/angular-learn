9070c34a236caa2ee68d2e0a4dedccedb391c31d
ramden <ramden@gmail.com>
2018-07-15 13:37:02 +0200

    initial

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout/layout.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LayoutComponent, NavbarComponent, FooterComponent],
+  exports: [LayoutComponent]
+})
+export class CoreModule { }
